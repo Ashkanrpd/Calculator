3ede198cf59cff63a89bacbc8e0c74da8170be7d
ashkan <azarqam@gmail.com>
2020-07-22 11:45:26 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState("");
+  const [secondNum, setSecondNum] = useState("");
+  const [action, setAction] = useState("");
+  const [history, setHistory] = useState([]);
+
+  function clear() {
+    setResult("");
+    setSecondNum("");
+    setAction("");
+    setHistory([]);
+  }
+
+  function assign(num) {
+    if (!action) {
+      setResult(result + "" + num);
+    } else setSecondNum(secondNum + "" + num);
+  }
+
+  function doMath() {
+    if (!secondNum) return;
+    setHistory([...history, result]);
+    switch (action) {
+      case "+":
+        setResult(Number(result) + Number(secondNum));
+        break;
+      case "-":
+        setResult(Number(result) - Number(secondNum));
+        break;
+      case "*":
+        setResult(Number(result) * Number(secondNum));
+        break;
+      case "/":
+        setResult(Number(result) / Number(secondNum));
+        break;
+    }
+    setSecondNum("");
+    setHistory([secondNum, action, result, "---", ...history]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="keysAndResult">
+        <p className="result">{secondNum ? secondNum : result}</p>
+        <div className="keys">
+          <button onClick={() => assign(1)}>1</button>
+          <button onClick={() => assign(2)}>2</button>
+          <button onClick={() => assign(3)}>3</button>
+          <button onClick={() => setAction("+")}>+</button>
+        </div>
+        <div className="keys">
+          <button onClick={() => assign(4)}>4</button>
+          <button onClick={() => assign(5)}>5</button>
+          <button onClick={() => assign(6)}>6</button>
+          <button onClick={() => setAction("-")}>-</button>
+        </div>
+        <div className="keys">
+          <button onClick={() => assign(7)}>7</button>
+          <button onClick={() => assign(8)}>8</button>
+          <button onClick={() => assign(9)}>9</button>
+          <button onClick={() => setAction("*")}>*</button>
+        </div>
+        <div className="keys">
+          <button onClick={() => assign(".")}>.</button>
+          <button onClick={() => assign(0)}>0</button>
+          <button onClick={() => setAction("/")}>/</button>
+          <button onClick={() => doMath()}>=</button>
+        </div>
+        <div>
+          <button className="clear" onClick={() => clear()}>
+            Clear
+          </button>
+        </div>
+      </div>
+      <div className="history">
+        <div>History</div>
+        {history.map((x) => (
+          <div>{x}</div>
+        ))}
+      </div>
     </div>
   );
 }
